7a84b74a3553559bd30812f2640f203d8379d395
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2024-07-21 09:47:50 +0530

    creating footer sectio

--- index.js
+++ index.js
@@ -36,3 +36,24 @@ var swiper = new Swiper(".mySwiper", {
      clickable: true,
    },
  });
+
+
+
+ //scroll to top botton 
+
+ const heroSection = document.querySelector(".section-hero")
+
+ const footerElem = document.querySelector(".section-footer");
+
+ const scrollElement = document.createElement("div");
+ scrollElement.classList.add("scrollTop-style");
+
+ scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`;
+
+footerElem.after(scrollElement);
+
+const scrollTop = () => {
+  heroSection.scrollIntoView({behavior: "smooth"});
+};
+
+scrollElement.addEventListener("click", scrollTop);
